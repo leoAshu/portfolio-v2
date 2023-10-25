a2137deee7a1a9c8105e5a51d21af1371fbc4bcd
leoAshu <ashutosh.ojha2009@gmail.com>
2023-10-26 03:15:40 +0530

    updated experience section

--- src/assets/index.js
+++ src/assets/index.js
@@ -21,10 +21,10 @@ import tailwind from "./tech/tailwind.png";
 import typescript from "./tech/typescript.png";
 import threejs from "./tech/threejs.svg";
 
-import meta from "./company/meta.png";
-import shopify from "./company/shopify.png";
-import starbucks from "./company/starbucks.png";
-import tesla from "./company/tesla.png";
+import oit from "./company/oit.png";
+import issc from "./company/issc.png";
+import amazon from "./company/amazon.png";
+import enparadigm from "./company/enparadigm.png";
 
 import carrent from "./carrent.png";
 import jobit from "./jobit.png";
@@ -52,10 +52,10 @@ export {
   tailwind,
   typescript,
   threejs,
-  meta,
-  shopify,
-  starbucks,
-  tesla,
+  oit,
+  issc,
+  amazon,
+  enparadigm,
   carrent,
   jobit,
   tripguide,
