dfe88e885ffe6adff075ce23ee8b87bb72d4f435
leoAshu <ashutosh.ojha2009@gmail.com>
2023-10-26 02:42:43 +0530

    updates about section

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -19,7 +19,7 @@ const ServiceCard = ({ index, title, icon }) => {
             scale: 1,
             speed: 450
           }}
-          className="bg-tertiary rounded-[20px] py-5 px-12 min-h-[280px] flex justify-evenly items-center flex-col"
+          className="bg-tertiary rounded-[20px] py-5 min-h-[280px] flex justify-evenly items-center flex-col"
         >
           <img src={icon} alt={title} className="w-16 h-16 object-contain"/>
           <h3 className="text-white text-[20px] font-bold text-center" >{title}</h3>
@@ -41,9 +41,8 @@ const About = () => {
         variants={fadeIn("", "", 0.1, 1)}
         className="mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]"
       >
-        I'm a skilled software developer with experience in TypeScript and JavaScript with an expertise in frameworks like React, Node.js, and Three.js.
-        I'm a quick learner and collaborate closely with clients to create efficient, scalable, and user-friendly solutions that solve real-world problems.
-        Let's work together to bring your ideas to life!
+        I'm a skilled software developer with experience in Python, TypeScript, JavaScript and Java including frameworks like Flask, React, Node.js, Flutter, and Tensorflow. <br />
+        My dedication to lifelong learning enables me to swiftly adapt to any tech stack, making a positive impact.
       </motion.p>
 
       <div className="mt-20 flex flex-wrap gap-10">
--- src/constants/index.js
+++ src/constants/index.js
@@ -46,7 +46,7 @@ import {
       icon: web,
     },
     {
-      title: "React Native Developer",
+      title: "Mobile App Developer",
       icon: mobile,
     },
     {
@@ -54,7 +54,7 @@ import {
       icon: backend,
     },
     {
-      title: "Content Creator",
+      title: "Machine Learning Engineer",
       icon: creator,
     },
   ];
