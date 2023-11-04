234522576513e1a6688413d80fb6060963344141
leoAshu <ashutosh.ojha2009@gmail.com>
2023-11-04 14:10:29 +0530

    fixes hero design for mobile screens

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -25,7 +25,7 @@ const Computers = ({ isMobile }) => {
       <primitive 
         object={computer.scene} 
         scale={isMobile ? 0.5 :  0.75}
-        position={isMobile ? [0, -3, -1.8] : [0, -3, -1.5]}
+        position={isMobile ? [0, -3.2, -1.6] : [0, -3, -1.5]}
         rotation={[-0.01, -0.2, -0.1]}
       />
     </mesh>
